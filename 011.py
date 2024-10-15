bbf9085876e29c2c5850d7bd08ebb0eb7f7c2463
Sharada Kamble <119296988+SHkamble@users.noreply.github.com>
2024-10-15 18:56:14 +0530

    Add files via upload

---
+++ 011.py
@@ -0,0 +1,21 @@
+# Operators
+#Arithmatic operators--- ADD, SUBTRACT, MULTIPLY.DIVISION,
+# MODULUS (REMINDER), EXPONENTIALS,FLOOR DIVISION(RETURN THE QUOTIENT, DISCARDING THE REMINDER)
+#comparision
+#logical
+#assignmend
+#membership
+#Identity
+
+# can we take value from user?
+
+a = int(input("Enter the value of a\n"))#2
+b = int(input("Enter the value of b\n"))#4
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b) #2^4
+print(a//b)# 2/4 0
---
+++ 012.py
@@ -0,0 +1,16 @@
+# comparisn operator :
+#== equal to
+#!= not equal to
+#< less than
+#>  greater than
+# <=less than equal to
+# >= more than equal to.
+
+a = 2
+b = 8  # boll --> true or false
+
+print(2==8)
+print(2 > 8)
+print(2 < 8)
+print(2 >= 8)
+print(2 <= 8)
