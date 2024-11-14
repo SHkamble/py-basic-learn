e34d5f1f9700f62b14621eee7e2ca816fe3cdf95
Sharada Kamble <119296988+SHkamble@users.noreply.github.com>
2024-11-14 19:03:19 +0530

    Add files via upload

---
+++ 022.py
@@ -0,0 +1,13 @@
+#condition
+# set of rules - > age > 18 then you can watch a movie
+
+age= int (input("enter your age"))
+print(age)
+
+# if this
+# else then
+
+if age > 18:
+    print("you can watch movie")
+else:
+    print("you can't")
---
+++ 025.py
@@ -0,0 +1,17 @@
+ # Loops
+# repeatation a block of code multiple times
+""" print("hello")
+ print("hello")
+ print("hello")
+ print("hello")
+ print("hello")
+ print("hello")"""
+
+for x in range (1,6,2):
+      print(x)
+
+for u in range(1, 10,2):
+          print(u)
+
+ # range means (start , stop , step)
+ #16 october 41:19
