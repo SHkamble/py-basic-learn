9abd9afb5a55e4db0182010f70a3cd056b11fee6
Sharada Kamble <119296988+SHkamble@users.noreply.github.com>
2024-11-16 17:49:03 +0530

    Add files via upload

---
+++ 000.0.py
@@ -0,0 +1,6 @@
+#Range..Start,stop,step
+x = list(range(10))
+print(x)
+
+for x in range(1,10,2):
+    print(x)
---
+++ 000.1.py
@@ -0,0 +1,5 @@
+# while loop
+i = 0
+while i < 5: #conditioned - true or False
+    print(i)
+    i = i +1  #increment
