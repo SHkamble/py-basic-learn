5890fda5c0882b9c73eb3ea26717685e836286d9
Sharada Kamble <119296988+SHkamble@users.noreply.github.com>
2024-12-03 19:10:28 +0530

    Add files via upload

---
+++ 048.py
@@ -0,0 +1,16 @@
+# List
+my_list = [1,1,2,3,45,6,6]
+my_list2 = [1,True,"sharda"]
+
+print("Initial list:",my_list)
+
+# Indexing
+print("Element at index 0:",my_list[0])
+
+#Changing an element
+my_list[1] =20
+print("List after changing element at index 1:", my_list)
+
+# append()
+my_list.append(4)
+print("List after appending:", my_list)
