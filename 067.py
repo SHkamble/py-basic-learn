b800e596177201f99c6a2277b3942d2a60fb0439
Sharada Kamble <119296988+SHkamble@users.noreply.github.com>
2024-12-17 18:58:23 +0530

    Add files via upload

---
+++ 067.py
@@ -0,0 +1,25 @@
+
+
+#Automation tester - API and web automation
+# python  -vast - Not all the functions are required to built your framework
+# to even crack the interview
+
+
+
+
+
+my_dict = {'b':2,'a':1,'c': 3}
+for k,v in my_dict.items():
+    if k == 'b':
+        print("b is found!!")
+
+
+op = 'b' in my_dict
+op2 = 'd' in my_dict
+print(op)
+print(op2)
+
+my_dict2 = {True : 123}
+print(my_dict2)
+print(type(my_dict2))
+print(my_dict2[True])
---
+++ 068.py
@@ -0,0 +1,5 @@
+my_dict = {}
+keyword= input("Enter a key:")
+value = input("Enter a value:")
+my_dict[keyword]= value
+print("Updated dictionary:",my_dict)
